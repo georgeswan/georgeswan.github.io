3ed17b1aa7a3e75c375c8d0f4782c208872df961
George Swan <112808384+georgeswan@users.noreply.github.com>
2023-04-22 18:14:38 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -15,14 +15,14 @@ let myHeading = document.querySelector("h1");
 function setUserName() {
     const myName = prompt("Please enter your name.");
     localStorage.setItem("name", myName);
-    myHeading.textContent = `Mozilla is cool, ${myName}`;
+    myHeading.textContent = `Welcome to my website, ${myName}!`;
   }
 
   if (!localStorage.getItem("name")) {
     setUserName();
   } else {
     const storedName = localStorage.getItem("name");
-    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+    myHeading.textContent = `Welcome to my website, ${myName}!`;
   }
 
   myButton.onclick = () => {
@@ -34,7 +34,7 @@ function setUserName() {
       setUserName();
     } else {
       localStorage.setItem("name", myName);
-      myHeading.textContent = `Mozilla is cool, ${myName}`;
+      myHeading.textContent = `Welcome to my website, ${myName}!`;
     }
   }
   
